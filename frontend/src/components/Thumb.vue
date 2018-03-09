e7e6e8845efb70125efbb1dbd12c95829f1599f5
Peter <peter@otocolobus.se>
2018-03-09 19:04:28 +0100

    Indicate when user makes a vote #5

---
+++ frontend/src/components/Thumb.vue
@@ -0,0 +1,31 @@
+<template>
+  <font-awesome :icon="icons.thumbsUp" class="thumb"/>
+</template>
+
+
+<script>
+  import FontAwesome from '@fortawesome/vue-fontawesome'
+  import {faThumbsUp} from '@fortawesome/fontawesome-free-solid'
+
+  export default {
+    components: {
+      FontAwesome
+    },
+    computed: {
+      icons () {
+        return {
+          thumbsUp: faThumbsUp
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .thumb {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+</style>
--- frontend/src/components/User.vue
+++ frontend/src/components/User.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="userInfo">
+    <div class="userInfo" v-bind:class="{voting: voting}">
         <img class="profilePicture"
-             v-bind:class="{voting: voting}"
              :src="profilePicture"
              :title="user.name"/>
-        <span class="username">{{user.name}}</span>
-      </div>
+      <span class="username" @click="test()">{{user.name}}</span>
+      <transition-group name="fade" tag="p">
+        <thumb v-for="t in thumbs" :key="t"/>
+      </transition-group>
+    </div>
 </template>
 
 <script>
   import { gravatar } from '../gravatar.js'
   import {EventBus} from '../EventBus'
+  import Thumb from './Thumb.vue'
 
   export default {
     data () {
       return {
-        voting: false
+        voting: false,
+        thumbs: []
       }
     },
     props: ['user', 'picturesize'],
@@ -24,14 +28,21 @@
         return gravatar.profilePicture(this.user, 100)
       }
     },
+    components: {
+      Thumb
+    },
     created () {
       var that = this
       EventBus.$on('USER_VOTED', (info) => {
-        if (this.$store.state.me && info.user_id === this.$store.state.me.id) {
-          return
-        }
         if (info.user_id === this.user.id) {
-          that.voting = !that.voting
+          that.thumbs.push(Date.now())
+          setTimeout(() => {
+            that.thumbs.shift()
+          }, 0)
+          that.voting = true
+          setTimeout(() => {
+            that.voting = false
+          }, 500)
         }
       })
     }
@@ -46,7 +57,7 @@
     width: 1em;
   }
   .voting {
-    border: 5px solid red;
+    background-color: #efefef;
   }
   .username {
     text-overflow: ellipsis;
@@ -55,6 +66,17 @@
   }
   .userInfo {
     border-bottom: 1px solid #ddd;
+    position: relative;
+  }
+
+  .fade-leave-active {
+    transition: opacity 2s;
+  }
+  .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    transition: all 2s;
+    transform: scale(2) translate(5em);
+    color: #0074D9;
   }
 
 </style>
